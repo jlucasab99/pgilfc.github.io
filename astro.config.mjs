18363e2ac90927a90a366224afa34252d881cdbc
Lucas Brito <a84595@alunos.uminho.pt>
2023-03-13 15:37:06 +0000

    add partydown in integrations into astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,21 @@ import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://afonsina.com",
-  integrations: [svelte(), image(), prefetch(), tailwind(), sitemap()],
+  integrations: [
+		svelte(),
+		image(),
+		prefetch(),
+		tailwind(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		})
+	],
 });
